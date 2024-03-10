09657f8739000c8218ce8a6b2e1601e63485d411
amy <tjfdbfls12@naver.com>
2024-03-11 00:20:21 +0900

    first commit

---
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity 	
+public class SecurityConfig {
+	
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/user/**").authenticated()
+			.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.anyRequest().permitAll()
+			.and()
+			.formLogin()
+			.loginPage("/login");
+		return http.build();
+	}
+}
---
+++ src/main/java/com/cos/security1/config/TestService.java
@@ -0,0 +1,8 @@
+package com.cos.security1.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+}
